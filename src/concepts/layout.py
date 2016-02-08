919a86d83bf20e7798f9c4b81c38f2ac2d22cbaa
Lazar Nikolić <lazarpwna@gmail.com>
2016-02-08 20:34:53 +0100

    Uvedena podrska za layout i semanticka provera za pozicije. Dva ugradjena layouta: border i grid.

---
+++ src/concepts/layout.py
@@ -0,0 +1,17 @@
+'''
+Created on 08.02.2016.
+
+@author: Lazar
+'''
+
+class Layout(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, name, positions, parent):
+        self.name = name
+        self.positions = positions
+        self.parent = parent
+        
--- src/interpreter/interpreter.py
+++ src/interpreter/interpreter.py
@@ -5,19 +5,28 @@ Created on 03.02.2016.
 '''
 from textx.metamodel import metamodel_from_file
 
+from concepts.layout import Layout
 from concepts.object import Object
 from concepts.property import Property
 from concepts.selector_object import SelectorObject, selector_object_processor
 from concepts.selector_view import SelectorView
 from concepts.view import View
+from concepts.page import page_processor
 
 
 if __name__ == "__main__":
     
-    basic_types = {x : View(None,x, []) for x in View.basic_type_names}
+    builtins = {x : View(None,x, []) for x in View.basic_type_names}
+    layouts = {
+               'border' : Layout('border', ['top','bottom','center','left','right'], None),
+               'grid' : Layout('grid', [], None)
+               }
+    
+    builtins.update(layouts) 
     
     obj_processors = {
-                      'SelectorObject' : selector_object_processor
+                      'SelectorObject' : selector_object_processor,
+                      'Page' : page_processor
                       }
     
     my_mm = metamodel_from_file('../grammar/grammar.tx',
@@ -28,7 +37,7 @@ if __name__ == "__main__":
                                             SelectorObject, 
                                             SelectorView
                                             ],
-                                 builtins = basic_types,
+                                 builtins = builtins,
                                  debug = False)
     
     my_mm.register_obj_processors(obj_processors)
