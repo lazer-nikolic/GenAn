38b85cfcd955decd885b7008524809c2d405b47e
drilic <ilic.dragoljub.92@gmail.com>
2016-02-16 16:36:30 +0100

    Classes paremeters added to templates, changed all templates to call macro messageAlert if contains required class

--- src/concepts/property.py
+++ src/concepts/property.py
@@ -3,16 +3,27 @@ Created on 08.02.2016.
 
 @author: Lazar
 '''
-
+def property_processor(property_object):
+    classesString ="";
+    if not property_object.classes is None:
+        for x in property_object.classes.htmlClasses:
+            if hasattr(x, 'value'):
+                classesString+=" "+x.key+"=\""+x.value+"\"";
+            else:
+                classesString+=" "+x.key;
+        property_object.classes=classesString
+    else:
+        property_object.classes=""
+        
 class Property(object):
     '''
     classdocs
     '''
 
-    def __init__(self, name, type, param, params, label, parent, classes):
+    def __init__(self, name, type, ref_param, params, label, parent, classes):
         self.name = name
         self.type = type
-        self.param = param
+        self.ref_param = ref_param
         self.label = label
         self.parent = parent       
         self.params = params       
