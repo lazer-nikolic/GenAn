390742db3642a77c7170ae0583555eebbe2e6163
BojanaZoranovic <bojana.zoranovic@hotmail.com>
2016-02-22 23:33:07 +0100

    Renaming factory template

---
+++ src/generation/templates/controllers/factory.js
@@ -0,0 +1,44 @@
+{% import 'common.jinja' as common %}
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('{{object.name | title}}Factory', {{object.name | title}}Factory);
+
+    {{object.name | title}}Factory.$inject = ['ApiService'];
+
+    function {{object.name | title}}Factory(ApiService) {
+
+        var service = this;
+        var relativeUrl = "{{object.name}}";
+
+        function getAll() {
+            return ApiService.getAll(relativeUrl);
+        }
+
+        function getById(id) {
+            return ApiService.getById(relativeUrl, id);
+        }
+
+        function create(single_{{object.name}}) {
+            return ApiService.create(relativeUrl, single_{{object.name}}, single_{{object.name}}._id);
+        }
+
+        function update(single_{{object.name}}) {
+            return ApiService.update(relativeUrl, single_{{object.name}}, single_{{object.name}}._id);
+        }
+
+        function remove(id) {
+            return ApiService.remove(relativeUrl, id);
+        }
+
+        return {
+            getAll: getAll,
+            getById: getById,
+            create: create,
+            update: update,
+            remove: remove
+        };
+    }
+})();
