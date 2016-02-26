4a2b7c4bb23822b35810bdec5d04918506c4ea9d
BojanaZoranovic <bojana.zoranovic@hotmail.com>
2016-02-26 07:51:27 +0100

    Minor fixes for displaying form values

--- src/generation/templates/controllers/form.js
+++ src/generation/templates/controllers/form.js
@@ -3,45 +3,38 @@
     'use strict';
 
     angular.module('app')
-<<<<<<< HEAD
-        .controller('{{form.name | title}}_formController', {{form.name | title}}_formController);
-
-    {{form.name | title}}_formController.$inject = ['EventBus','{{form.name | title}}Factory', '$filter', '$stateParams'];
-
-    function {{form.name | title}}_formController(EventBus, {{form.name | title}}Factory, $filter, $stateParams) {
-=======
         .controller('{{name | title}}formController', {{name | title}}formController);
 
     {{name | title}}formController.$inject = ['EventBus','{{name | title}}Factory', '$filter', '$stateParams'];
 
     function {{name | title}}formController(EventBus, {{name | title}}Factory, $filter, $stateParams) {
->>>>>>> aa82ef6078860aca7847f0bc6c84daaa1a084309
+
 
         var ctrl = this;
         ctrl.update = false;
         ctrl.oldObject = {};
-        ctrl.currentObject = {};
+        ctrl.{{name}} = {};
 
-        if ($stateParams.id != 'new') {
+        if ($stateParams.id  !== undefined && $stateParams.id != "") {
             {{name | title}}Factory.getById($stateParams.id).then(function(success) {
-                ctrl.currentObject = success;
-                angular.copy(ctrl.currentObject, ctrl.oldObject);
+                ctrl.{{name}} = success;
+                angular.copy(ctrl.{{name}}, ctrl.oldObject);
                 ctrl.update = true;
             });
         }
 
         ctrl.save = function () {
             if (ctrl.update) {
-                {{name | title}}Factory.update(ctrl.currentObject);
-                angular.copy(ctrl.currentObject, ctrl.oldObject);
+                {{name | title}}Factory.update(ctrl.{{name}});
+                angular.copy(ctrl.{{name}}, ctrl.oldObject);
             } else {
-                {{name | title}}Factory.save(ctrl.currentObject);
-                angular.copy(ctrl.currentObject, ctrl.oldObject);
+                {{name | title}}Factory.create(ctrl.{{name}});
+                angular.copy(ctrl.{{name}}, ctrl.oldObject);
             }
         }
 
         ctrl.resetUpdate = function () {
-            angular.copy(ctrl.oldObject, ctrl.currentObject);
+            angular.copy(ctrl.oldObject, ctrl.{{name}});
         }
 
         ctrl.cancel = function () {
@@ -49,7 +42,7 @@
         }
 
         ctrl.remove = function () {
-            {{name | title}}Factory.remove(ctrl.currentObject.id);
+            {{name | title}}Factory.remove(ctrl.{{name}}.id);
             EventBus.emitEvent('GoToTableUser');
         }
     {%for x in formInputs %}
