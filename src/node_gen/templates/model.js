4ff5b8643754f78a136ea95e398d540f00265867
drilic <ilic.dragoljub.92@gmail.com>
2016-06-20 11:01:48 +0200

    Changed node_generator to generate foreign keys for objects.

--- src/node_gen/templates/model.js
+++ src/node_gen/templates/model.js
@@ -4,6 +4,13 @@ var {{o.name|title}}Schema = new mongoose.Schema({
 {% for prop in o.properties %}
   {{prop.name}}: {{ persistent_types[prop.type.name] if prop.type.name in persistent_types else 'String' }},
 {% endfor %}
+{% for fk in o.meta%}
+ {%if fk.foreignKeyType == 'list'%}
+  {{fk.label}} : [{ type: Schema.Types.ObjectId, ref: {{fk.object.name|title}} }],
+ {%else%}
+  {{fk.label}} : { type: Schema.Types.ObjectId, ref: {{fk.object.name|title}} },
+ {%endif%}
+{%endfor%}
   updated_at: { type: Date, default: Date.now },
 });
 
